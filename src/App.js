86fd68d710eb2727a6b55e40911ab0b91a5db291
Vinicius <vroliveira.eng@gmail.com>
2025-01-09 16:34:15 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import logo from './imagens/logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+          <p><strong>Alura Books</strong> </p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
