e59becc59981f79b2b063c70196598ed761801d9
Vinicius <vroliveira.eng@gmail.com>
2025-01-11 13:01:21 -0300

    Serviço GET de favoritos

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,9 @@
 import Input from '../Input';
 import styled from 'styled-components';
-import { useState } from 'react';
-import { livros } from './dadosPesquisa';
+import { useEffect, useState } from 'react';
+import { getLivros } from '../../servicos/livros';
+
+
 
 const PesquisaContainer = styled.section`
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
@@ -23,26 +25,34 @@ const SubTitulo = styled.h3`
   margin-bottom: 40px;
 `;
 const Resultado = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 20px;
-    cursor: pointer;
-    p {
-        width: 200px;
-    }
-    img {
-        width: 100px;
-    }
-    &:hover {
-        border: 1px solid white;
-    }
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+  p {
+    width: 200px;
+  }
+  img {
+    width: 100px;
+  }
+  &:hover {
+    border: 1px solid white;
+  }
+`;
 
 function Pesquisa() {
   const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+  const [ livros, setLivros ] = useState([]);
 
-  console.log(livrosPesquisados);
+  useEffect(() => {
+    fetchLivros()
+  }, [])
+
+  async function fetchLivros() {
+    const livrosDaAPI = await getLivros()
+    setLivros(livrosDaAPI)
+  }
 
   return (
     <PesquisaContainer>
@@ -57,23 +67,12 @@ function Pesquisa() {
           );
           setLivrosPesquisados(resultadoPesquisa);
         }}
-        onKeyDown={(evento) => {
-          if (evento.key === 'Enter') {
-            const textoDigitado = evento.target.value;
-            const resultadoPesquisa = livros.filter((livro) =>
-              livro.nome.includes(textoDigitado)
-            );
-            setLivrosPesquisados(resultadoPesquisa);
-          }
-        }}
       />
       {livrosPesquisados.map((livro) => (
         <Resultado>
-        <img src={livro.src} 
-             alt='livro.src'
-             />
-        <p>{livro.nome}</p>
-    </Resultado>
+          <img src={livro.src} alt="livro.src" />
+          <p>{livro.nome}</p>
+        </Resultado>
       ))}
     </PesquisaContainer>
   );
