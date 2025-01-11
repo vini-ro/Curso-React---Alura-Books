1f69eddfb06898d8ee9400387f2213741d6c7929
Vinicius <vroliveira.eng@gmail.com>
2025-01-11 10:24:02 -0300

    Primeira rota com React Router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createGlobalStyle } from 'styled-components';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -22,13 +23,20 @@ code {
 li {
     list-style: none;
   }
+
+
 `;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/favoritos" element={<p>oi!</p>} />
+        <Route path="/" element={<App />} />
+      </Routes>
+      </BrowserRouter >
   </React.StrictMode>
 );
 
